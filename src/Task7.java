cf9584cb6023b9ab65fe76372fe58b41e9a39e2c
Kalidolda Damilya <dkalidolda@gmail.com>
2026-03-24 08:41:01 +0500

    task7

---
+++ src/Task7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task7 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        printReverse(n);
+        System.out.println();
+        sc.close();
+    }
+
+    static void printReverse(int n) {
+        if (n == 0) return;
+        int num = sc.nextInt();
+        printReverse(n - 1);
+        System.out.print(num + " ");
+    }
+}
